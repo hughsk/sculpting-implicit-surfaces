74dfb2943c825edf9994d77087d00583bcb3ebcc
Hugh Kennedy <hughskennedy@gmail.com>
2016-01-16 22:14:17 +1100

    more talk

--- src/slideshow.js
+++ src/slideshow.js
@@ -1,17 +1,23 @@
+import Fit from 'canvas-fit'
 import sliced from 'sliced'
 
 export default (wrapper) => new Slideshow(wrapper)
 
 class Slideshow {
   constructor (wrapper) {
+    this.canvas = document.createElement('canvas')
+    this.gl = this.canvas.getContext('webgl')
+    this.transitionTimer = null
+
     this.slideWrapper = wrapper
     this.slideElements = sliced(wrapper.querySelectorAll('section[data-slide]'))
     this.totalSlides = this.slideElements.length
-    this.currSlide = 0
-
+    this.currSlide = parseInt(window.localStorage.getItem('implicit-slide'), 10) || 0
     this.slideEvents = {}
 
+    this.fitter = Fit(this.canvas)
     this.resize()
+
     window.addEventListener('resize', () => this.resize(), false)
     window.addEventListener('keydown', (e) => {
       switch (e.keyCode) {
@@ -23,8 +29,14 @@ class Slideshow {
         case 39:
         case 40:
           return this.nextSlide()
+
+        case 192:
+          document.body.parentElement.classList.toggle('editor-enabled')
+          this.fitter()
+          return
       }
     }, false)
+
   }
 
   nextSlide () {
@@ -35,12 +47,31 @@ class Slideshow {
     this.toSlide((this.currSlide + this.totalSlides - 1) % this.totalSlides)
   }
 
-  toSlide (n) {
-    this.currSlide = n
-    this.slideWrapper.style.top = (-100 * n) + 'vh'
+  toSlide (nextSlide) {
+    const prev = this.slideElements[this.currSlide]
+    const next = this.slideElements[nextSlide]
+
+    prev.classList.remove('current')
+    next.classList.add('current')
+
+    this.currSlide = nextSlide
+    this.slideWrapper.style.top = (-100 * nextSlide) + 'vh'
+
+    window.localStorage.setItem('implicit-slide', String(this.currSlide))
+
+    if (this.transitionTimer) clearTimeout(this.transitionTimer)
+
+    const canvasShell = next.querySelector('.canvas-shell')
+    this.transitionTimer = setTimeout(() => {
+      if (!canvasShell) return
+
+      canvasShell.appendChild(this.canvas)
+      this.fitter()
+    }, 500)
   }
 
   resize () {
     this.toSlide(this.currSlide)
+    this.fitter()
   }
 }
